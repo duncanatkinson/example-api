196d18ba29d3ccd3ccd7c13e130c5dcfb27b1ac3
Duncan Atkinson <duncan.atkinson@nationaltrust.org.uk>
2017-05-31 11:21:14 +0100

    initial

---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("duncanatkinson:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("duncanatkinson:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("duncanatkinson.sample.service")
+class Start {
+}
