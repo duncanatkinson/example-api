695e2ef440e7c747779b24bfa7ce8c3a9105304d
Duncan Atkinson <duncan.atkinson@nationaltrust.org.uk>
2017-05-31 16:39:00 +0100

    switched to a cleaner newer starting point

--- start.groovy
+++
@@ -1,5 +0,0 @@
-@Grab("duncanatkinson:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
-@Grab("duncanatkinson:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
-@ComponentScan("duncanatkinson.sample.service")
-class Start {
-}
