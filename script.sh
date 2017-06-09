811140e33587002646753211c08b0573848f06c2
Duncan Atkinson <duncan.atkinson@nationaltrust.org.uk>
2017-06-09 13:51:03 +0100

    test should fake in the right dir

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 echo $USER
 
 mkdir -p example-api/build/libs/
-touch example-api/build/libs/example-api.jar
+touch build/libs/example-api.jar
 
 echo contents of example-api
 ls example-api
 
-echo contents of example-api/build/libs
-ls example-api/build/libs
+echo contents of build/libs
+ls build/libs
 
 echo contents of file version/version
 cat version/version
