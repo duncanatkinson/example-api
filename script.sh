3f98ab2238f52a17563e96c9fd914ac95764940a
Duncan Atkinson <duncan.atkinson@nationaltrust.org.uk>
2017-06-09 10:46:53 +0100

    wip

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@ echo $USER
 
 ls -lart
 
-cat version/version
+
 ls example-api
 mkdir -p example-api/build/libs/
 touch example-api/build/libs/example-api.jar
 ls example-api/build/libs
+cat version/version
